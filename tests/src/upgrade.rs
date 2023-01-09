4089f5c31be789918991ae95962100b3476ce072
Deusz <lovaszoltanr@gmail.com>
2023-01-09 17:06:58 +0100

    migrate test

--- tests/src/upgrade.rs
+++ tests/src/upgrade.rs
@@ -2,15 +2,16 @@ use casper_engine_test_support::{
     ExecuteRequestBuilder, InMemoryWasmTestBuilder, DEFAULT_ACCOUNT_ADDR,
     DEFAULT_RUN_GENESIS_REQUEST,
 };
-use casper_types::{account::AccountHash, runtime_args, CLValue, Key, RuntimeArgs};
+use casper_types::{account::AccountHash, runtime_args, CLValue, ContractHash, Key, RuntimeArgs};
 
 use crate::utility::{
     constants::{
-        ACCESS_KEY_NAME, ACCOUNT_USER_1, ARG_IS_HASH_IDENTIFIER_MODE, ARG_NFT_CONTRACT_HASH,
-        ARG_NFT_PACKAGE_HASH, ARG_SOURCE_KEY, ARG_TARGET_KEY, ARG_TOKEN_HASH, ARG_TOKEN_META_DATA,
-        ARG_TOKEN_OWNER, CONTRACT_1_0_0_WASM, MINT_SESSION_WASM, NFT_CONTRACT_WASM,
-        NFT_TEST_COLLECTION, NFT_TEST_SYMBOL, PAGE_LIMIT, PAGE_SIZE, RECEIPT_NAME,
-        TRANSFER_SESSION_WASM, UNMATCHED_HASH_COUNT, UPDATED_RECEIPTS_WASM,
+        ACCESS_KEY_NAME, ACCOUNT_USER_1, ARG_CONTRACT_WHITELIST, ARG_IS_HASH_IDENTIFIER_MODE,
+        ARG_NFT_CONTRACT_HASH, ARG_NFT_PACKAGE_HASH, ARG_SOURCE_KEY, ARG_TARGET_KEY,
+        ARG_TOKEN_HASH, ARG_TOKEN_META_DATA, ARG_TOKEN_OWNER, CONTRACT_1_0_0_WASM, CONTRACT_NAME,
+        MINT_SESSION_WASM, NFT_CONTRACT_WASM, NFT_TEST_COLLECTION, NFT_TEST_SYMBOL, PAGE_LIMIT,
+        PAGE_SIZE, RECEIPT_NAME, TRANSFER_SESSION_WASM, UNMATCHED_HASH_COUNT,
+        UPDATED_RECEIPTS_WASM,
     },
     installer_request_builder::{
         InstallerRequestBuilder, MetadataMutability, NFTIdentifierMode, NFTMetadataKind,
@@ -477,4 +478,65 @@ fn should_not_migrate_contracts_with_zero_token_issuance() {
     let error = builder.get_error().expect("must have error");
 
     support::assert_expected_error(error, 122u16, "cannot upgrade when issuance is 0");
+}
+
+#[test]
+fn should_safely_upgrade_contract_whitelist() {
+    let mut builder = InMemoryWasmTestBuilder::default();
+    builder.run_genesis(&DEFAULT_RUN_GENESIS_REQUEST).commit();
+
+    let whitelisted_contract_hash = ContractHash::from([1_u8; 32]);
+
+    let install_request = InstallerRequestBuilder::new(*DEFAULT_ACCOUNT_ADDR, CONTRACT_1_0_0_WASM)
+        .with_collection_name(NFT_TEST_COLLECTION.to_string())
+        .with_collection_symbol(NFT_TEST_SYMBOL.to_string())
+        .with_total_token_supply(1u64)
+        .with_ownership_mode(OwnershipMode::Minter)
+        .with_identifier_mode(NFTIdentifierMode::Ordinal)
+        .with_nft_metadata_kind(NFTMetadataKind::Raw)
+        .with_contract_whitelist_1_0_0(vec![whitelisted_contract_hash])
+        .build();
+
+    builder.exec(install_request).expect_success().commit();
+
+    let account = builder.get_expected_account(*DEFAULT_ACCOUNT_ADDR);
+    let nft_contract_key = account
+        .named_keys()
+        .get(CONTRACT_NAME)
+        .expect("must have key in named keys");
+
+    let contract_whitelist_pre_upgrade: Vec<ContractHash> = support::query_stored_value(
+        &mut builder,
+        *nft_contract_key,
+        vec![ARG_CONTRACT_WHITELIST.to_string()],
+    );
+
+    assert_eq!(
+        contract_whitelist_pre_upgrade,
+        vec![whitelisted_contract_hash],
+        "contract whitelist is incorrectly set"
+    );
+
+    let upgrade_request = ExecuteRequestBuilder::standard(
+        *DEFAULT_ACCOUNT_ADDR,
+        NFT_CONTRACT_WASM,
+        runtime_args! {
+            ARG_NFT_PACKAGE_HASH => support::get_nft_contract_package_hash(&builder),
+        },
+    )
+    .build();
+
+    builder.exec(upgrade_request).expect_success().commit();
+
+    let post_upgrade_contract_whitelist: Vec<Key> = support::query_stored_value(
+        &mut builder,
+        *nft_contract_key,
+        vec![ARG_CONTRACT_WHITELIST.to_string()],
+    );
+
+    assert_eq!(
+        post_upgrade_contract_whitelist,
+        vec![whitelisted_contract_hash.into()],
+        "contract whitelist is incorrectly set"
+    );
 }
